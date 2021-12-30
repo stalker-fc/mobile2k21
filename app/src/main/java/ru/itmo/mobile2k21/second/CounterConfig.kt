f7faaf5c6a34ad8ea8c508e37ca85dd9e5b00b05
stalker-fc <stalker-fc@yandex.ru>
2021-12-30 12:57:14 +0300

    update counter_view

--- app/src/main/java/ru/itmo/mobile2k21/second/CounterConfig.kt
+++ app/src/main/java/ru/itmo/mobile2k21/second/CounterConfig.kt
@@ -1,7 +1,6 @@
 package ru.itmo.mobile2k21.second
 
 data class CounterConfig(
-    val counterId: Int,
     var currentUpdateIntervalMs: Long,
     val changeIntervalDeltaMs: Long,
 )
--- app/src/main/java/ru/itmo/mobile2k21/second/CounterView.kt
+++ app/src/main/java/ru/itmo/mobile2k21/second/CounterView.kt
@@ -1,7 +1,34 @@
 package ru.itmo.mobile2k21.second
 
-class CounterView : ICounterView {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.itmo.mobile2k21.R
+
+class CounterView : AppCompatActivity(), ICounterView {
+    private lateinit var presenter: ICounterPresenter
+    private lateinit var counterLabel: TextView
+    private lateinit var slowDownButton: Button
+    private lateinit var speedUpButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.counter_item)
+
+        counterLabel = findViewById(R.id.counter_label)
+        slowDownButton = findViewById(R.id.counter_slow_down)
+        speedUpButton = findViewById(R.id.counter_speed_up)
+
+        slowDownButton.setOnClickListener { presenter.slowDown() }
+        speedUpButton.setOnClickListener { presenter.speedUp() }
+    }
+
     override fun setLabelValue(value: Int) {
-//        counterLabel.text = value.toString()
+        counterLabel.text = value.toString()
+    }
+
+    fun setPresenter(presenter: ICounterPresenter) {
+        this.presenter = presenter
     }
 }
