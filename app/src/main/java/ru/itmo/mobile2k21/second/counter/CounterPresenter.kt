9e2cd6999c32b04e21d691c8554f26f611badcd5
stalker-fc <stalker-fc@yandex.ru>
2022-01-25 00:01:19 +0300

    add true threads to task 2

--- app/src/main/java/ru/itmo/mobile2k21/second/counter/CounterPresenter.kt
+++ app/src/main/java/ru/itmo/mobile2k21/second/counter/CounterPresenter.kt
@@ -2,9 +2,6 @@ package ru.itmo.mobile2k21.second.counter
 
 import android.os.Handler
 import android.os.Looper
-import ru.itmo.mobile2k21.second.counter.CounterModel
-import ru.itmo.mobile2k21.second.counter.ICounterPresenter
-import ru.itmo.mobile2k21.second.counter.ICounterView
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.max
@@ -20,33 +17,35 @@ class CounterPresenter(
 
     private val isRunning: AtomicBoolean = AtomicBoolean(false)
 
-    private val counterThread: Runnable = object : Runnable {
-        override fun run() {
-            if (isRunning.get()) {
-                counterModel.value.getAndIncrement()
-                updateCounterLabel()
-                mainHandler.postDelayed(this, currentUpdateIntervalMs)
-            }
+    private val counterRunnable: Runnable = Runnable {
+        while (isRunning.get()) {
+            counterModel.value.getAndIncrement()
+            mainHandler.post(counterLabelUpdater)
+            Thread.sleep(currentUpdateIntervalMs)
         }
     }
+
+    private val counterLabelUpdater: Runnable = Runnable {
+        counterView.setLabelValue(counterModel.value.get())
+    }
+
 
     override fun start() {
         if (isRunning.get()) {
             return
         }
         isRunning.set(true)
-        mainHandler.post(counterThread)
+        Thread(counterRunnable).start()
     }
 
     override fun stop() {
         isRunning.set(false)
-        mainHandler.removeCallbacks(counterThread)
     }
 
     override fun reset() {
         stop()
         counterModel.value.set(0)
-        updateCounterLabel()
+        mainHandler.post(counterLabelUpdater)
     }
 
     override fun slowDown() {
@@ -59,9 +58,4 @@ class CounterPresenter(
             this.changeIntervalDeltaMs
         )
     }
-
-    private fun updateCounterLabel() {
-        counterView.setLabelValue(counterModel.value.get())
-    }
-
 }
